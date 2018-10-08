77833089747ee1ca5e8f74564aa7b75b2212770e
Lena <lena4ka94@inbox.ru>
2018-10-08 21:34:46 +0300

    Завершила класс Route

--- Route.rb
+++ Route.rb
@@ -5,24 +5,42 @@
 # Может выводить список всех станций по-порядку от начальной до конечной
 
 class Route
-  def initislize(initial_station, final_station, intermediate_stations = [])
+  # Метод конструктор
+  def initialize(initial_station, final_station, intermediate_stations = [])
     @initial_station = initial_station
     @final_station = final_station
     @intermediate_stations = intermediate_stations
   end
-  +
 
+  # Добавить промежуточную станцию
   def add_intermediate_station(station)
     @intermediate_stations.push(station)
   end
 
+  # Удалить промежуточную станцию
   def remove_intermediate_station(station)
     @intermediate_stations.delete(station)
   end
 
+  # Выводит список станций в консоль
   def display_route
     puts @initial_station
     puts @intermediate_stations
     puts @final_station
   end
+
+  # Возвращает массив со всеми станциями маршрута
+  def station_list
+    stations = [@initial_station, @intermediate_stations, @final_station]
+    return stations
+  end
 end
+
+#route = Route.new("Koptevo", "Bot Sad", ["Hovrino", "Sokol"])
+#route.display_route
+#route.add_intermediate_station("Dinamo")
+#route.display_route
+#route.remove_intermediate_station("Hovrino")
+#route.display_route
+#stations = route.station_list
+#puts stations
